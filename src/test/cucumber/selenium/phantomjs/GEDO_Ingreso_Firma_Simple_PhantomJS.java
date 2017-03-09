1b734bda578891ad832366e60bd95293cac5edfb
gautocarlos <cargauto@BUE-79QLQC2.usersad.everis.int>
2017-03-09 14:56:33 -0300

    Refactor espera de elementos
    Se factoriza en un método la espera de elementos buscando mediante el
    xpath de los mismos.
    Se eliminan comentarios innecesarios dentro del código.

--- src/test/cucumber/selenium/phantomjs/GEDO_Ingreso_Firma_Simple_PhantomJS.java
+++ src/test/cucumber/selenium/phantomjs/GEDO_Ingreso_Firma_Simple_PhantomJS.java
@@ -35,9 +35,10 @@ public class GEDO_Ingreso_Firma_Simple_PhantomJS {
 		try {
 			driver = new PhantomJSDriver(caps);
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			System.out.println("Pasó");
+			System.out.println("Inicialización Driver PhantomJS: SIN ERRORES");
 		} catch (Exception e) {
-			System.out.println("Falló");
+			System.out.println("Inicialización Driver PhantomJS: ERROR");
+			throw e;
 		}
 	}
 
@@ -49,70 +50,62 @@ public class GEDO_Ingreso_Firma_Simple_PhantomJS {
 
 	@When("^Ingresa a la URL \"(.*)\"$")
 	public void ingresoCAS(String url) throws Exception {
-		System.out.println("URL: " + url);
+		// System.out.println("URL: " + url);
 		driver.navigate().to(url);
 		wait = new WebDriverWait(driver, 20);
-		System.out.println("driver.getCurrentUrl():: " + driver.getCurrentUrl());
+		// System.out.println("driver.getCurrentUrl():: " +
+		// driver.getCurrentUrl());
 	}
 
 	@When("^El usuario ingresa al sistema con sus credenciales: usuario \"(.*)\" y password \"(.*)\"$")
 	public void datosLogin(String usuario, String password) throws Exception {
-		System.out.println("datosLogin - Ini :: driver.getCurrentUrl():: " + driver.getCurrentUrl());
+		// System.out.println("datosLogin - Ini :: driver.getCurrentUrl():: " +
+		// driver.getCurrentUrl());
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[2]/div/input")).clear();
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[2]/div/input")).sendKeys(usuario);
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[3]/div/input")).clear();
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[3]/div/input"))
 				.sendKeys(password);
 		driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div/div/div/div/div[4]/button")).click();
-		System.out.println("datosLogin - Fin :: driver.getCurrentUrl():: " + driver.getCurrentUrl());
+		// System.out.println("datosLogin - Fin :: driver.getCurrentUrl():: " +
+		// driver.getCurrentUrl());
 	}
 
 	@When("^El usuario firma una tarea de firma simple con certificado$")
 	public void testGEDOIngresoFirmaSimple() throws Exception {
 		// driver.navigate().to("http://eug.nac.gde.gob.ar/gedo-web/");
 		wait = new WebDriverWait(driver, 30);
-		System.out.println("driver.getCurrentUrl():: " + driver.getCurrentUrl());
+		//		System.out.println("driver.getCurrentUrl():: " + driver.getCurrentUrl());
 		Thread.sleep(3000);
-		for (int second = 0;; second++) {
-			if (second >= 60)
-				fail("timeout");
-			try {
-				if (isElementPresent(By.xpath("//td/img")))
-					break;
-			} catch (Exception e) {
-			}
-			Thread.sleep(2000);
-		}
-
+		waitElementIsPresentByXpath("//td/img");
 		driver.findElement(By.xpath("//td/img")).click();
-		for (int second = 0;; second++) {
-			if (second >= 60)
-				fail("timeout");
-			try {
-				if (isElementPresent(By.xpath("//td[5]/div/div/div/img")))
-					break;
-			} catch (Exception e) {
-			}
-			Thread.sleep(2000);
-		}
-
+		waitElementIsPresentByXpath("//td[5]/div/div/div/img");
 		driver.findElement(By.xpath("//td[5]/div/div/div/img")).click();
 		/*
 		 * Cerrar cartel de firma de documento y volver a la bandeja de inicio
 		 */
+		waitElementIsPresentByXpath("//td[2]/div/div/div/div/img");
+		driver.findElement(By.xpath("//td[2]/div/div/div/div/img")).click();
+
+	}
+
+	/**
+	 * @param xpath
+	 * @throws Exception
+	 * Espera a que un elemento aparezca
+	 * Busca el mismo mediante su xpath
+	 */
+	private void waitElementIsPresentByXpath(String xpath) throws Exception {
 		for (int second = 0;; second++) {
 			if (second >= 60)
 				fail("timeout");
 			try {
-				if (isElementPresent(By.xpath("//td[2]/div/div/div/div/img")))
+				if (isElementPresent(By.xpath(xpath)))
 					break;
 			} catch (Exception e) {
 			}
-			Thread.sleep(1000);
+			Thread.sleep(2000);
 		}
-
-		driver.findElement(By.xpath("//td[2]/div/div/div/div/img")).click();
-
 	}
 
 	@Then("^Se genera un número de documento GDE$")
